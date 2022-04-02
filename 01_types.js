d705a6bb9fd79e814dc0bf06cc40a679cdd658e9
Arseniy Boltzmunn <ars.boltz@gmail.com>
2022-04-02 13:59:16 +0300

    types

--- 01_types.js
+++ 01_types.js
@@ -9,3 +9,15 @@ console.log(typeof Math)
 console.log(typeof Symbol('JS'))
 console.log(typeof null) // является типом данных NULL
 console.log(typeof NaN)
+
+//приведение типов
+
+let language = 'JavaScript'
+
+if (language) {
+    console.log('The best language is:', language)
+}
+
+//строки и числа
+
+console.log(1 + '2')
