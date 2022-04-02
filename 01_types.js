0a09e3efa592c51b49117636a893c4099a429b9b
Arseniy Boltzmunn <ars.boltz@gmail.com>
2022-04-02 13:51:57 +0300

    startTasks

---
+++ 01_types.js
@@ -0,0 +1,11 @@
+// null, undefined, boolean, number, string, object, symbol
+
+
+console.log(typeof(0))
+console.log(typeof true)
+console.log(typeof 'JavaScript')
+console.log(typeof undefined)
+console.log(typeof Math)
+console.log(typeof Symbol('JS'))
+console.log(typeof null) // является типом данных NULL
+console.log(typeof NaN)
